1edef6d662f539d826d9b99a989ae2fb54f7d768
秋水 <1123993881@qq.com>
2022-06-05 16:51:43 +0800

    Add project files.

---
+++ PlaceHolderComponentEditor/PlaceHolderComponentEditorInfo.cs
@@ -0,0 +1,28 @@
+﻿using Grasshopper;
+using Grasshopper.Kernel;
+using System;
+using System.Drawing;
+
+namespace PlaceHolderComponentEditor
+{
+    public class PlaceHolderComponentEditorInfo : GH_AssemblyInfo
+    {
+        public override string Name => "Place Holder Component Editor";
+
+        //Return a 24x24 pixel bitmap to represent this GHA library.
+        public override Bitmap Icon => null;
+
+        //Return a short string describing the purpose of this GHA library.
+        public override string Description => "Make place holder componennt editable!";
+
+        public override Guid Id => new Guid("5C788E3B-E279-43E6-A7C0-B88CF226C298");
+
+        //Return a string identifying you or your company.
+        public override string AuthorName => "秋水";
+
+        //Return a string representing your preferred contact details.
+        public override string AuthorContact => "1123993881@qq.com";
+
+        public override string Version => "0.9.0";
+    }
+}
