0f1d29fb82d661868c2fb5df55293c5179a30be9
秋水 <1123993881@qq.com>
2022-06-06 22:52:05 +0800

    first release.

---
+++ PlaceHolderComponentEditor/GH_EditablePlaceholderComponentAttributes.cs
@@ -0,0 +1,88 @@
+﻿using Grasshopper.Kernel;
+using Grasshopper.Kernel.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grasshopper.Kernel.Components;
+using Grasshopper.GUI.Canvas;
+using System.Drawing.Drawing2D;
+using Grasshopper.GUI;
+using Eto.Forms;
+
+namespace PlaceHolderComponentEditor
+{
+    public class GH_EditablePlaceholderComponentAttributes : GH_ComponentAttributes
+    {
+		public override bool AllowMessageBalloon => false;
+		public override RectangleF Bounds { get; set; }
+		public override PointF Pivot { get; set; }
+
+        private PointF lastPivot;
+
+		public GH_EditablePlaceholderComponentAttributes(GH_Component owner)
+			: base(owner)
+		{
+			lastPivot = Pivot = owner.Attributes.Pivot;
+			Bounds = owner.Attributes.Bounds;
+			Selected = owner.Attributes.Selected;
+		}
+
+		protected override void Layout()
+		{
+			PointF dir = Pivot.Subtract(lastPivot);
+			lastPivot = Pivot;
+
+			Bounds = Bounds.MoveRectangleF(dir);
+
+            foreach (var item in Owner.Params)
+            {
+				item.Attributes.Bounds = item.Attributes.Bounds.MoveRectangleF(dir);
+			}
+		}
+
+        public override void ExpireLayout()
+        {
+			//MessageBox.Show("Expired!");
+            base.ExpireLayout();
+        }
+
+
+        protected override void Render(GH_Canvas canvas, Graphics graphics, GH_CanvasChannel channel)
+		{
+			switch (channel)
+			{
+				case GH_CanvasChannel.Wires:
+					base.Render(canvas, graphics, channel);
+					break;
+				case GH_CanvasChannel.Objects:
+					{
+						int num = base.Owner.Params.Input.Count - 1;
+						for (int i = 0; i <= num; i++)
+						{
+							PointF inputGrip = base.Owner.Params.Input[i].Attributes.InputGrip;
+							graphics.FillEllipse(rect: new RectangleF(inputGrip.X - 4f, inputGrip.Y - 4f, 8f, 8f), brush: Brushes.DimGray);
+						}
+						int num2 = base.Owner.Params.Output.Count - 1;
+						for (int j = 0; j <= num2; j++)
+						{
+							PointF outputGrip = base.Owner.Params.Output[j].Attributes.OutputGrip;
+							graphics.FillEllipse(rect: new RectangleF(outputGrip.X - 4f, outputGrip.Y - 4f, 8f, 8f), brush: Brushes.DimGray);
+						}
+						GraphicsPath graphicsPath = GH_CapsuleRenderEngine.CreateRoundedRectangle(GH_Convert.ToRectangle(Bounds), 3);
+						Pen pen = new Pen(Color.DimGray, 1f)
+						{
+							DashStyle = DashStyle.Dash
+						};
+						graphics.FillPath(Selected ? new SolidBrush(GH_Skin.wire_selected_a) : Brushes.White, graphicsPath);
+						graphics.DrawPath(pen, graphicsPath);
+						graphicsPath.Dispose();
+						pen.Dispose();
+						break;
+					}
+			}
+		}
+	}
+}
---
+++ PlaceHolderComponentEditor/PlaceHolderComponentEditorPriority.cs
@@ -0,0 +1,84 @@
+﻿using Eto.Forms;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaceHolderComponentEditor
+{
+    public class PlaceHolderComponentEditorPriority : GH_AssemblyPriority
+    {
+        public override GH_LoadingInstruction PriorityLoad()
+        {
+            Grasshopper.Instances.CanvasCreated += Instances_CanvasCreated;
+            return GH_LoadingInstruction.Proceed;
+        }
+
+        private void Instances_CanvasCreated(Grasshopper.GUI.Canvas.GH_Canvas canvas)
+        {
+            Grasshopper.Instances.CanvasCreated -= Instances_CanvasCreated;
+            canvas.DocumentChanged += Canvas_DocumentChanged;
+
+            ExchangeMethod(
+                typeof(GH_Document).GetRuntimeMethods().Where(m => m.Name.Contains("RelevantObjectAtPoint") && m.GetParameters().Length == 2).First(),
+                typeof(DrawingHelper).GetRuntimeMethods().Where(m => m.Name.Contains("RelevantObjectAtPoint")).First()
+            );
+
+        }
+
+
+        private void Canvas_DocumentChanged(Grasshopper.GUI.Canvas.GH_Canvas sender, Grasshopper.GUI.Canvas.GH_CanvasDocumentChangedEventArgs e)
+        {
+            //MessageBox.Show("changed!");
+            if (e.NewDocument == null) return;
+            foreach (var item in e.NewDocument.Objects)
+            {
+                if (item.GetType().FullName.Contains("Grasshopper.Kernel.Components.GH_PlaceholderComponent") && item is GH_Component compnent)
+                {
+                    compnent.Attributes = new GH_EditablePlaceholderComponentAttributes(compnent);
+                }
+                else if (item.GetType().FullName.Contains("Grasshopper.Kernel.Components.GH_PlaceholderParameter") && item is GH_Param<IGH_Goo> param)
+                {
+                    param.Attributes = new GH_EditablePlaceholderFloatingAttributes(param);
+                }
+            }
+        }
+
+        internal static bool ExchangeMethod(MethodInfo targetMethod, MethodInfo injectMethod)
+        {
+            if (targetMethod == null || injectMethod == null)
+            {
+                return false;
+            }
+            RuntimeHelpers.PrepareMethod(targetMethod.MethodHandle);
+            RuntimeHelpers.PrepareMethod(injectMethod.MethodHandle);
+            unsafe
+            {
+                if (IntPtr.Size == 4)
+                {
+                    int* tar = (int*)targetMethod.MethodHandle.Value.ToPointer() + 2;
+                    int* inj = (int*)injectMethod.MethodHandle.Value.ToPointer() + 2;
+                    var relay = *tar;
+                    *tar = *inj;
+                    *inj = relay;
+                }
+                else
+                {
+                    long* tar = (long*)targetMethod.MethodHandle.Value.ToPointer() + 1;
+                    long* inj = (long*)injectMethod.MethodHandle.Value.ToPointer() + 1;
+                    var relay = *tar;
+                    *tar = *inj;
+                    *inj = relay;
+                }
+            }
+            return true;
+        }
+
+    }
+}
